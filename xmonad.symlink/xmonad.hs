dbaec7be255af3db7e38e6d3ca685922c950b8d2
David Pedersen <david.pdrsn@gmail.com>
2015-03-01 18:24:30 +0100

    More linux config

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -1,6 +1,12 @@
 import XMonad
 
+main :: IO ()
 main = do
+  spawn "xbindkeys"
+  spawn "prevent-death"
+  spawn "dunst"
+  spawn "xset -b"
+
   xmonad $ defaultConfig { borderWidth = 2
                          , normalBorderColor = "#111111"
                          , focusedBorderColor = "#333333"
