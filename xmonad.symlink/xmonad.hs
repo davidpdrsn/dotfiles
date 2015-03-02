b5161462e7fa8c5bcb03ddb04d1a58cda51c36c3
David Pedersen <david.pdrsn@gmail.com>
2015-03-02 15:10:44 +0100

    Add xmonad maps for seeing time and battery status

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Util.EZConfig
 
 main :: IO ()
 main = do
@@ -6,8 +7,13 @@ main = do
   spawn "prevent-death"
   spawn "dunst"
 
-  xmonad $ defaultConfig { borderWidth = 2
-                         , normalBorderColor = "#111111"
-                         , focusedBorderColor = "#333333"
-                         , terminal = "urxvt"
-                         }
+  xmonad $ def { borderWidth = 2
+               , normalBorderColor = "#111111"
+               , focusedBorderColor = "#333333"
+               , terminal = "urxvt"
+               } `additionalKeys` myKeys
+
+myKeys :: [((KeyMask, KeySym), X ())]
+myKeys = [ ((mod1Mask, xK_b), spawn "notify-send \"`acpi`\"")
+         , ((mod1Mask, xK_d), spawn "notify-send \"`date`\"")
+         ]
